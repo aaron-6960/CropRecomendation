dfd803b31932d5eeaed23a8614d4cdd158ded36c
aaron-6960 <138325645+aaron-6960@users.noreply.github.com>
2024-07-31 22:48:12 +0530

    Server

--- runserver.py
+++ runserver.py
@@ -1,6 +1,6 @@
-from backend.myapi import app
-import uvicorn 
+import os
+import uvicorn
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 8000))  # Default to 8000 if PORT is not set
-    uvicorn.run(app, host="0.0.0.0", port=port, reload=True)
+    uvicorn.run("backend.myapi:app", host="0.0.0.0", port=port, reload=True)
