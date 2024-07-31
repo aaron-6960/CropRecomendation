1d122411337632430c39be6763969925c1299820
aaron-6960 <138325645+aaron-6960@users.noreply.github.com>
2024-07-31 22:41:40 +0530

    added port details

--- runserver.py
+++ runserver.py
@@ -2,4 +2,5 @@ from backend.myapi import app
 import uvicorn 
 
 if __name__ == "__main__":
-    uvicorn.run("backend.myapi:app", port=8000, reload=True)
+    port = int(os.getenv("PORT", 8000))  # Default to 8000 if PORT is not set
+    uvicorn.run(app, host="0.0.0.0", port=port, reload=True)
