9bf9a040e2f598546decc889590d5db0fec032fa
aaron-6960 <138325645+aaron-6960@users.noreply.github.com>
2024-08-02 22:53:27 +0530

    Completed

--- frontend/src/components/crop.jsx
+++ frontend/src/components/crop.jsx
@@ -1,92 +1,79 @@
-import React, { useContext } from 'react';
-import './crop.css';
-import { useNavigate } from 'react-router-dom'; 
+import React, { useContext } from "react";
+import "./crop.css";
+import { useNavigate } from "react-router-dom";
 import { FeaturesContext } from "../context/featuresContext";
 
-function Crop() {
-  const navigate = useNavigate();  
+// Import all images
+import appleImage from "../pictures/apple.png";
+import bananaImage from "../pictures/banana.jpg";
+import blackgramImage from "../pictures/blackgram.png";
+import chickpeaImage from "../pictures/chickpea.png";
+import coconutImage from "../pictures/coconut.png";
+import coffeeImage from "../pictures/coffee.png";
+import cottonImage from "../pictures/cotton.png";
+import grapesImage from "../pictures/grapes.png";
+import juteImage from "../pictures/jute.png";
+import kidneybeansImage from "../pictures/kidneybeans.png";
+import lentilImage from "../pictures/lentil.png";
+import maizeImage from "../pictures/maize.png";
+import mangoImage from "../pictures/mango.png";
+import mothbeansImage from "../pictures/mothbeans.png";
+import mungbeansImage from "../pictures/mungbeans.png";
+import muskmelonImage from "../pictures/muskmelon.png";
+import orangeImage from "../pictures/orange.png";
+import papayaImage from "../pictures/papaya.png";
+import pigeonpeasImage from "../pictures/pigeonpeas.png";
+import pomegranateImage from "../pictures/pomegranate.png";
+import riceImage from "../pictures/rice.png";
+import watermelonImage from "../pictures/watermelon.png";
 
+function Crop() {
+  const navigate = useNavigate();
   const { crop } = useContext(FeaturesContext);
 
   const submitHandler = (e) => {
-    e.preventDefault();  
-    navigate('/home');  
+    e.preventDefault();
+    navigate("/home");
   };
 
-  let backgroundImage;
-  switch (crop) {
-    case 'apple':
-      backgroundImage = '/src/pictures/apple.png';
-      break;
-    case 'banana':
-      backgroundImage = '/src/pictures/banana.jpg';
-      break;
-    case 'blackgram':
-      backgroundImage = '/src/pictures/blackgram.png';
-      break;
-    case 'chickpea':
-      backgroundImage = '/src/pictures/chickpea.png';
-      break;
-    case 'coconut':
-      backgroundImage = '/src/pictures/coconut.png';
-      break;
-    case 'coffee':
-      backgroundImage = '/src/pictures/coffee.png';
-      break;
-    case 'cotton':
-      backgroundImage = '/src/pictures/cotton.png';
-      break;
-    case 'grapes':
-      backgroundImage = '/src/pictures/grapes.png';
-      break;
-    case 'jute':
-      backgroundImage = '/src/pictures/jute.png';
-      break;
-    case 'kidneybeans':
-      backgroundImage = '/src/pictures/kidneybeans.png';
-      break;
-    case 'lentil':
-      backgroundImage = '/src/pictures/lentil.png';
-      break;
-    case 'maize':
-      backgroundImage = '/src/pictures/maize.png';
-      break;
-    case 'mango':
-      backgroundImage = '/src/pictures/mango.png';
-      break;
-    case 'mothbeans':
-      backgroundImage = '/src/pictures/mothbeans.png';
-      break;
-    case 'mungbeans':
-      backgroundImage = '/src/pictures/mungbeans.png';
-      break;
-    case 'muskmelon':
-      backgroundImage = '/src/pictures/muskmelon.png';
-      break;
-    case 'orange':
-      backgroundImage = '/src/pictures/orange.png';
-      break;
-    case 'papaya':
-      backgroundImage = '/src/pictures/papaya.png';
-      break;
-    case 'pigeonpeas':
-      backgroundImage = '/src/pictures/pigeonpeas.png';
-      break;
-    case 'pomegranate':
-      backgroundImage = '/src/pictures/pomegranate.png';
-      break;
-    case 'rice':
-      backgroundImage = '/src/pictures/rice.png';
-      break;
-    case 'watermelon':
-      backgroundImage = '/src/pictures/watermelon.png';
-      break;
-  }
+  // Map crop types to imported images
+  const cropImages = {
+    apple: appleImage,
+    banana: bananaImage,
+    blackgram: blackgramImage,
+    chickpea: chickpeaImage,
+    coconut: coconutImage,
+    coffee: coffeeImage,
+    cotton: cottonImage,
+    grapes: grapesImage,
+    jute: juteImage,
+    kidneybeans: kidneybeansImage,
+    lentil: lentilImage,
+    maize: maizeImage,
+    mango: mangoImage,
+    mothbeans: mothbeansImage,
+    mungbeans: mungbeansImage,
+    muskmelon: muskmelonImage,
+    orange: orangeImage,
+    papaya: papayaImage,
+    pigeonpeas: pigeonpeasImage,
+    pomegranate: pomegranateImage,
+    rice: riceImage,
+    watermelon: watermelonImage,
+  };
+
+  // Get the image based on the crop type
+  const backgroundImage = cropImages[crop];
 
   return (
-    <div className="Crop" style={{ backgroundImage: `url(${backgroundImage})` }}>
+    <div
+      className="Crop"
+      style={{ backgroundImage: `url(${backgroundImage})` }}
+    >
       <div className="CropHeading">
-        <button onClick={submitHandler} className='ReturnHomeButton'>Return To Home</button>
+        <button onClick={submitHandler} className="ReturnHomeButton">
+          Return To Home
+        </button>
       </div>
     </div>
   );
