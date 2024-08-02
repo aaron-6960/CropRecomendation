743305b621273c11db9eab96ceffd8ab30c25f55
aaron-6960 <138325645+aaron-6960@users.noreply.github.com>
2024-08-02 21:22:28 +0530

    reduced image size

--- backend/myapi.py
+++ backend/myapi.py
@@ -1,9 +1,10 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, BackgroundTasks
 from pydantic import BaseModel
-from .prediction import predict_crop
 import logging
+import httpx
+import asyncio
 from fastapi.middleware.cors import CORSMiddleware
-
+from .prediction import predict_crop
 
 app = FastAPI()
 
@@ -20,7 +21,6 @@ class Features(BaseModel):
     ph: float
     rainfall: float
 
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # List of origins allowed to make requests to this API
@@ -29,12 +29,31 @@ app.add_middleware(
     allow_headers=["*"],  # List of HTTP headers allowed
 )
 
+# Function to ping another server
+async def ping_server():
+    while True:
+        try:
+            async with httpx.AsyncClient() as client:
+                response = await client.get("https://scam-1.onrender.com/")
+                if response.status_code == 200:
+                    logger.info("Successfully pinged the server.")
+                else:
+                    logger.error(f"Failed to ping the server: {response.status_code}")
+        except Exception as e:
+            logger.error(f"Error pinging the server: {e}")
+        await asyncio.sleep(600)  # Wait for 10 minutes
+
+# Start the ping server in the background
+@app.on_event("startup")
+async def startup_event():
+    asyncio.create_task(ping_server())
+
 @app.get("/")
 def index():
     return {"message": "Welcome to the Crop Prediction API"}
 
 @app.post("/", response_model=dict)
-def prediction(data: Features):    
+def prediction(data: Features):
     try:
         result = predict_crop(data.N, data.P, data.K, data.temperature, data.humidity, data.ph, data.rainfall)
         logger.info(f"Prediction result: {result}")
@@ -42,4 +61,3 @@ def prediction(data: Features):
     except Exception as e:
         logger.error(f"Error during prediction: {e}")
         raise HTTPException(status_code=500, detail="Internal Server Error")
-    
